36380094dfd75e5d2ebcbb122600c3a0ee8dac27
ramteen <happinin@gmail.com>
2016-04-19 22:17:14 +1000

    new files

---
+++ GoogleScholar/src/mainAppWindow.java
@@ -0,0 +1,44 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Font;
+
+public class mainAppWindow {
+
+	private JFrame frmGoogleScholarTool;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					mainAppWindow window = new mainAppWindow();
+					window.frmGoogleScholarTool.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public mainAppWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmGoogleScholarTool = new JFrame();
+		frmGoogleScholarTool.setFont(new Font("Dialog", Font.BOLD, 12));
+		frmGoogleScholarTool.setTitle("Google Scholar Tool");
+		frmGoogleScholarTool.setBounds(100, 100, 780, 485);
+		frmGoogleScholarTool.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
