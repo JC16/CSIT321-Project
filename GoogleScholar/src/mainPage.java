52614aa368f092dc6e37828537a85fe2db2ea52f
YiTaiChen <jonathanchen16@gmail.com>
2016-05-28 18:50:27 +1000

    Main Page Design

---
+++ GoogleScholar/src/mainPage.java
@@ -0,0 +1,214 @@
+import java.awt.BorderLayout;
+import java.awt.Choice;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+public class mainPage extends JFrame {
+
+	private JFrame frmGoogleScholarTool;
+
+	//Menu Bar and Menu Item
+	private final JMenuBar menuBar = new JMenuBar();
+	private final JMenu mnFile = new JMenu("File");
+	private final JMenuItem mntmNewMenuItem = new JMenuItem("Import Data");
+	private final JMenuItem mntmNewMenuItem_1 = new JMenuItem("Save as CSV");
+	private final JMenuItem mntmExit = new JMenuItem("Exit");
+	private final JMenu mnEdit = new JMenu("Edit");
+	private final JMenuItem mntmCut = new JMenuItem("Cut");
+	private final JMenuItem mntmCopy = new JMenuItem("Copy");
+	private final JMenuItem mntmPaste = new JMenuItem("Paste");
+	private final JMenuItem mntmDelete = new JMenuItem("Delete");
+	private final JMenuItem mntmCopyResult = new JMenuItem("Copy Result");
+	private final JMenu mnView = new JMenu("View");
+	private final JMenuItem mntmJournalImpact = new JMenuItem("Journal Impact");
+	private final JMenuItem mntmAuthorImapct = new JMenuItem("Author Imapct");
+	private final JMenuItem mntmGeneralCitationSearch = new JMenuItem("General citation search");
+	private final JMenu mnTool = new JMenu("Tool");
+	private final JMenuItem mntmNewMenuItem_2 = new JMenuItem("Clear data");
+	private final JMenuItem mntmClearCache = new JMenuItem("Clear Cache");
+	private final JMenuItem mntmP = new JMenuItem("Preference");
+	private final JMenu mnHelp = new JMenu("Help");
+	private final JMenuItem mntmAboutThisProgram = new JMenuItem("About this program");
+	private final JLabel lblGoogleScholarSearch = new JLabel("Google Scholar Search Tool");
+	private final JButton btnNewButton = new JButton("New button");
+	private final JButton btnNewButton_1 = new JButton("New button");
+	private final JButton btnNewButton_2 = new JButton("New button");
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					mainPage frame = new mainPage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public mainPage() {
+		initialize();
+	}
+	
+	private void initialize() {
+		frmGoogleScholarTool = new JFrame();
+		frmGoogleScholarTool.setFont(new Font("Dialog", Font.BOLD, 12));
+		frmGoogleScholarTool.setTitle("Google Scholar Tool");
+		frmGoogleScholarTool.setBounds(100, 100, 999, 646);
+		frmGoogleScholarTool.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//JMenuBar menuBar = new JMenuBar();
+		frmGoogleScholarTool.setJMenuBar(menuBar);
+		
+		//JMenu mnNewMenu = new JMenu("File");
+		//menuBar.add(mnNewMenu);
+		
+		/*JMenuItem mntmExit = new JMenuItem("Exit");
+		mntmExit.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				System.exit(0);
+			}
+		});
+		mnNewMenu.add(mntmExit);
+		
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+		
+		JMenuItem mntmAbout = new JMenuItem("About");
+		mnHelp.add(mntmAbout);
+		*/
+		menuBar.add(mnFile);
+		
+		mnFile.add(mntmNewMenuItem);
+		
+		mnFile.add(mntmNewMenuItem_1);
+		
+		mnFile.add(mntmExit);
+		
+		menuBar.add(mnEdit);
+		
+		mnEdit.add(mntmCut);
+		
+		mnEdit.add(mntmCopy);
+		
+		mnEdit.add(mntmPaste);
+		
+		mnEdit.add(mntmDelete);
+		
+		mnEdit.add(mntmCopyResult);
+		
+		menuBar.add(mnView);
+		
+		mnView.add(mntmJournalImpact);
+		
+		mntmAuthorImapct.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				frmGoogleScholarTool.setVisible(false);
+				
+				authorSearch frame = new authorSearch();
+				frame.setVisible(true);
+				
+			}});
+		
+		
+		mnView.add(mntmAuthorImapct);
+		
+		mnView.add(mntmGeneralCitationSearch);
+		
+		menuBar.add(mnTool);
+		
+		mnTool.add(mntmClearCache);
+		
+		mnTool.add(mntmNewMenuItem_2);
+		
+		mnTool.add(mntmP);
+		
+		menuBar.add(mnHelp);
+		
+		mnHelp.add(mntmAboutThisProgram);
+		
+		
+		frmGoogleScholarTool.getContentPane().setLayout(null);
+		lblGoogleScholarSearch.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
+		lblGoogleScholarSearch.setBounds(313, 42, 449, 147);
+		
+		frmGoogleScholarTool.getContentPane().add(lblGoogleScholarSearch);
+		btnNewButton.setBounds(63, 238, 217, 203);
+		
+		frmGoogleScholarTool.getContentPane().add(btnNewButton);
+		btnNewButton_1.setBounds(387, 238, 217, 203);
+		
+		frmGoogleScholarTool.getContentPane().add(btnNewButton_1);
+		btnNewButton_2.setBounds(725, 241, 217, 203);
+		
+		frmGoogleScholarTool.getContentPane().add(btnNewButton_2);
+		
+		DefaultTableModel TableModel = new DefaultTableModel()
+		{
+			public Class<?> getColumnClass(int column)
+			{
+				switch(column)
+				{
+					case 0:
+						return String.class;
+					case 1:
+						return String.class;
+					case 2:
+						return String.class;
+					case 3:
+						return String.class;
+					case 4:
+						return String.class;
+						
+					default:
+							return String.class;
+				}
+			}
+		};
+		
+	}
+	
+	 
+	public void addColumn(DefaultTableModel TableModel, JTable table)
+	{
+		TableModel.addColumn("Cites");
+		TableModel.addColumn("Per year");
+		TableModel.addColumn("Rank");
+		TableModel.addColumn("Author");
+		TableModel.addColumn("Title");
+		TableModel.addColumn("Year");
+		TableModel.addColumn("Publication");
+		TableModel.addColumn("Publisher");
+		TableModel.addColumn("Type");
+	}
+}
