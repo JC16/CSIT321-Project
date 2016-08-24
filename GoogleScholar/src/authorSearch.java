88af557576e5c7a208c92e34d95e18e2f4808910
Scottlee <Scottlee@10.12.135.242>
2016-08-24 13:12:28 +1000

    Scott Test Commit

--- GoogleScholar/src/authorSearch.java
+++ GoogleScholar/src/authorSearch.java
@@ -28,7 +28,7 @@ import javax.swing.JTable;
 public class authorSearch extends JFrame {
 
 	private JPanel contentPane;
-	
+	//Hello
 	//test commit
 
 	private final JMenuBar menuBar = new JMenuBar();
