d255174a32764b226b5ed5a4aacd6fd84e0902b1
YiTaiChen <jonathanchen16@gmail.com>
2016-09-07 13:45:14 +1000

    main page interface update

--- GoogleScholar/src/mainPage.java
+++ GoogleScholar/src/mainPage.java
@@ -5,7 +5,10 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -26,11 +29,21 @@ import java.awt.FlowLayout;
 public class mainPage extends JFrame {
 
 	private JFrame frmGoogleScholarTool;
+	
+	
+	URL url = this.getClass().getResource("/icon/scholar.png");
+	Icon GSicon = new ImageIcon(url);
+	
+	URL sUrl = this.getClass().getResource("/icon/scopus.png");
+	Icon SpIcon = new ImageIcon(sUrl);
+	
 	private final JLabel lblGoogleScholarSearch = new JLabel("Scholar Search Tool");
-	private final JButton btnNewButton = new JButton("Google Scholar");
-	private final JButton btnNewButton_1 = new JButton("Scopus");
-	private final JButton btnNewButton_2 = new JButton("Search By General Information");
+	private final JButton btnNewButton = new JButton("Google Scholar",GSicon);
+	private final JButton btnNewButton_1 = new JButton("Scopus",SpIcon);
 	private final JPanel panel = new JPanel();
+	private final JLabel lblNewLabel = new JLabel("Run Scholar Search Tool");
+	private final JLabel lblNewLabel_1 = new JLabel("Select one search engine to start the program");
+	private final JLabel lblNewLabel_2 = new JLabel("Copyright (C) 2016 UOW GScholar Project. All Rights Reserved. Version 1.0.0");
 
 	/**
 	 * Launch the application.
@@ -76,7 +89,7 @@ public class mainPage extends JFrame {
 				
 			}
 		});
-		btnNewButton.setBounds(54, 310, 217, 203);
+		btnNewButton.setBounds(140, 244, 217, 76);
 		
 		frmGoogleScholarTool.getContentPane().add(btnNewButton);
 		btnNewButton_1.addActionListener(new ActionListener() {
@@ -89,22 +102,9 @@ public class mainPage extends JFrame {
 				
 			}
 		});
-		btnNewButton_1.setBounds(426, 310, 217, 203);
+		btnNewButton_1.setBounds(140, 359, 217, 76);
 		
 		frmGoogleScholarTool.getContentPane().add(btnNewButton_1);
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frmGoogleScholarTool.dispose();
-				
-				GeneralSearch GFrame = new GeneralSearch();
-				GFrame.setVisible(true);
-				
-				
-			}
-		});
-		btnNewButton_2.setBounds(813, 310, 217, 203);
-		
-		frmGoogleScholarTool.getContentPane().add(btnNewButton_2);
 		panel.setBackground(new Color(30, 144, 255));
 		panel.setBounds(0, 0, 1074, 166);
 		
@@ -114,6 +114,20 @@ public class mainPage extends JFrame {
 		lblGoogleScholarSearch.setBounds(358, 56, 390, 57);
 		panel.add(lblGoogleScholarSearch);
 		lblGoogleScholarSearch.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD, 25));
+		lblNewLabel.setForeground(new Color(0, 0, 255));
+		lblNewLabel.setBounds(498, 228, 340, 27);
+		
+		frmGoogleScholarTool.getContentPane().add(lblNewLabel);
+		lblNewLabel_1.setFont(new Font("Lucida Grande", Font.PLAIN, 17));
+		lblNewLabel_1.setForeground(new Color(0, 0, 255));
+		lblNewLabel_1.setBounds(498, 299, 382, 45);
+		
+		frmGoogleScholarTool.getContentPane().add(lblNewLabel_1);
+		lblNewLabel_2.setForeground(new Color(0, 0, 0));
+		lblNewLabel_2.setBounds(555, 664, 492, 16);
+		
+		frmGoogleScholarTool.getContentPane().add(lblNewLabel_2);
 		
 	}
 }
